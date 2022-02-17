358cdb9fbb895d88f2d9a3a7423a8e3f276f42e2
alihadous <hadous@umich.edu>
2022-02-17 01:45:47 -0500

    jenkins

--- Hello.java
+++ Hello.java
@@ -2,10 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     for(int i=1;i<=10;i++) {
       System.out.println("Hello World..."+i);
-      System.out.println("Hello World2..."+i);
-      System.out.println("Hello World3..."+i);
-
-
     }
   }
 }
